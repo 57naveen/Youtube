b28e82add7630aa2cf5537d3699b7712ef8c87b1
57naveen <naveenkishore65@gmail.com>
2025-10-31 08:48:14 +0530

    https update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const Google_API_KEY = "AIzaSyD0HGhVKjmX8_9PgkrRBK3VFkE2EfD0poc"
 
 export const  YOUTUBE_VIDEOS_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key="+ Google_API_KEY
 
-export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
+export const YOUTUBE_SEARCH_API="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
 
 export const LIVECHATCOUNT = 20
 
